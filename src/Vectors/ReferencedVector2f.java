15353fe165c32871bd028e0c1180ce5123d15721
NovaNix <47408522+NovaNix@users.noreply.github.com>
2019-04-04 12:25:59 -0500

    Added Hashcode support and fixed invert

--- src/Vectors/ReferencedVector2f.java
+++ src/Vectors/ReferencedVector2f.java
@@ -12,12 +12,14 @@ public class ReferencedVector2f extends Vector2f
 		
 		this.ReferencePoint = ReferencePoint;
 		this.Translation = Translation;
+		UpdateHashCode();
 	}
 	
 	public ReferencedVector2f(Vector2f ReferencePoint, Vector2f Translation)
 	{
 		this.ReferencePoint = ReferencePoint;
 		this.Translation = Translation;
+		UpdateHashCode();
 	}
 	
 	public ReferencedVector2f(String Name, Vector2f ReferencePoint)
@@ -26,33 +28,37 @@ public class ReferencedVector2f extends Vector2f
 		
 		this.ReferencePoint = ReferencePoint;
 		this.Translation = new Vector2f(0, 0);
+		UpdateHashCode();
 	}
 	
 	public ReferencedVector2f(Vector2f ReferencePoint)
 	{
 		this.ReferencePoint = ReferencePoint;
 		this.Translation = new Vector2f(0, 0);
+		UpdateHashCode();
 	}
 	
 	public void SetReference(Vector2f Reference)
 	{
 		this.ReferencePoint = Reference;
+		UpdateHashCode();
 	}
-	
+
 	@Override
-	public Vector2f Invert()
+	public void Invert()
 	{
-		return new ReferencedVector2f(ReferencePoint, Translation.Invert());
+		Translation.Invert();
+		UpdateHashCode();
 	}
 	
 	@Override
 	public void SetX(float X)
 	{
-
 		Vector2f NewPosition = new Vector2f(X, GetY());
 			
 		Add(new Vector2f(ReferencePoint.GetXDistanceTo(NewPosition), 0));
 		
+		// Add already updates hashcode, so updating it here is not necessary
 	}
 	
 	@Override
@@ -60,7 +66,9 @@ public class ReferencedVector2f extends Vector2f
 	{
 		Vector2f NewPosition = new Vector2f(GetX(), Y);
 			
-		Add(new Vector2f(0, ReferencePoint.GetYDistanceTo(NewPosition)));	
+		Add(new Vector2f(0, ReferencePoint.GetYDistanceTo(NewPosition)));
+				
+		// Add already updates hashcode, so updating it here is not necessary
 	}
 
 	@Override
@@ -79,24 +87,28 @@ public class ReferencedVector2f extends Vector2f
 	public void Add(Vector2f Change)
 	{
 		Translation.Add(Change);
+		UpdateHashCode();
 	}
 
 	@Override
 	public void Subtract(Vector2f Change)
 	{
 		Translation.Subtract(Change);
+		UpdateHashCode();
 	}
 
 	@Override
 	public void Multiply(Vector2f Change)
 	{
 		Translation.Multiply(Change);
+		UpdateHashCode();
 	}
 
 	@Override
 	public void Divide(Vector2f Change)
 	{
 		Translation.Divide(Change);
+		UpdateHashCode();
 	}
 	
 	public Vector2f Derive()
