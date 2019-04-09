c4b08f7bf2cbde479d9470aa6bdd6cd0af95abb5
Jennifer <Jennifer@LAPTOP-9574AILV>
2019-04-09 16:47:57 -0600

    Made new classes for OpenGL input

---
+++ src/IO/Keys/Mouse.java
@@ -0,0 +1,53 @@
+package IO.Keys;
+
+import Vectors.Vector2f;
+
+public class Mouse 
+{
+
+	public Mouse()
+	{
+		
+	}
+	
+	public void Refresh()
+	{
+		
+	}
+	
+	public void LeftMouseDown()
+	{
+		
+	}
+	
+	public void RightMouseDown()
+	{
+		
+	}
+	
+	public void MiddleMouseDown()
+	{
+		
+	}
+
+	public void LeftMouseClicked()
+	{
+		
+	}
+	
+	public void RightMouseClicked()
+	{
+		
+	}
+	
+	public void MiddleMouseClicked()
+	{
+		
+	}
+	
+	public Vector2f GetMousePosition()
+	{
+		return null;
+	}
+	
+}
