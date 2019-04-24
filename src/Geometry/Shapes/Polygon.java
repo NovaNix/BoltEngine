a464be908bfa8b5372f4f96249cdd56a5a37449e
michael <Jennifer@LAPTOP-9574AILV>
2019-04-23 19:31:46 -0600

    Added polygon splitting capabilities

--- src/Geometry/Shapes/Polygon.java
+++ src/Geometry/Shapes/Polygon.java
@@ -16,12 +16,16 @@ public class Polygon extends Shape
 
 	Vector2fGroup Corners;
 	Segmant[] Sides;
-
+	
 	Circle BoundingBubble;
 
+	protected float[] CompressedCorners;
+	protected int[] Index;
+	protected int[] TextureCoords;
+	
 	public Polygon(Vector2f[] Corners)
 	{
-		this.Position = new Vector2f(Vector2fUtils.GetFurthestWest(Corners).GetX(), Vector2fUtils.GetFurthestNorth(Corners).GetY());
+		this.Position = new Vector2f(Vector2fUtils.GetMinX(Corners).GetX(), Vector2fUtils.GetMaxY(Corners).GetY());
 
 		this.Corners = new Vector2fGroup(Corners);
 		this.Corners.ToRelative(Position);
@@ -59,8 +63,8 @@ public class Polygon extends Shape
 
 	public Vector2f[] GetCompressed()
 	{
-		Vector2f BottemLeft = new Vector2f(Vector2fUtils.GetFurthestWest(Corners.ToArray()).GetX(), Vector2fUtils.GetFurthestSouth(Corners.ToArray()).GetY());
-		Vector2f TopRight = new Vector2f(Vector2fUtils.GetFurthestEast(Corners.ToArray()).GetX(), Vector2fUtils.GetFurthestNorth(Corners.ToArray()).GetY());
+		Vector2f BottemLeft = new Vector2f(Vector2fUtils.GetMinX(Corners.ToArray()).GetX(), Vector2fUtils.GetMinY(Corners.ToArray()).GetY());
+		Vector2f TopRight = new Vector2f(Vector2fUtils.GetMaxX(Corners.ToArray()).GetX(), Vector2fUtils.GetMaxY(Corners.ToArray()).GetY());
 
 		float PolygonWidth = BottemLeft.GetXDistanceTo(TopRight);
 		float PolygonHeight = BottemLeft.GetYDistanceTo(TopRight);
@@ -77,10 +81,210 @@ public class Polygon extends Shape
 
 	}
 
+	public void Cut(Triangle Tri)
+	{
+		int C1 = Corners.GetVectorPosition(Tri.GetCorner1());
+		int C2 = Corners.GetVectorPosition(Tri.GetCorner2());
+		int C3 = Corners.GetVectorPosition(Tri.GetCorner3());
+		
+		int Mid = GetMiddlePoint(C1, C2, C3);
+		
+		Corners.RemoveVector(Corners.ToArray()[Mid]);
+	}
+	
+	private int GetMiddlePoint(int P1, int P2, int P3)
+	{
+		int[] Points = {P1, P2, P3};
+		
+		for (int i = 0; i < 3; i++)
+		{
+			int PreviousCorner;
+			int NextCorner;
+			
+			int SelectedPoint = Points[i];
+			
+			if (SelectedPoint == 0) 
+			{
+				PreviousCorner = Corners.ToArray().length - 1;
+				NextCorner = 1;
+			}
+
+			else if (SelectedPoint == Corners.ToArray().length - 1) 
+			{
+				PreviousCorner = SelectedPoint - 1;
+				NextCorner = 0;
+			}
+
+			else 
+			{
+				PreviousCorner = SelectedPoint - 1;
+				NextCorner = SelectedPoint + 1;
+			}
+			
+			if (i == 0)
+			{
+				if (Math.max(P2, P3) == Math.max(PreviousCorner, NextCorner) && Math.min(P2, P3) == Math.min(PreviousCorner, NextCorner))
+				{
+					return P1;
+				}
+			}
+			
+			else if (i == 1)
+			{
+				if (Math.max(P1, P3) == Math.max(PreviousCorner, NextCorner) && Math.min(P1, P3) == Math.min(PreviousCorner, NextCorner))
+				{
+					return P2;
+				}
+			}
+			
+			else if (i == 2)
+			{
+				if (Math.max(P2, P1) == Math.max(PreviousCorner, NextCorner) && Math.min(P2, P1) == Math.min(PreviousCorner, NextCorner))
+				{
+					return P3;
+				}
+			}
+		}
+		
+		System.out.println("Point not on polygon!");
+		
+		return 0;
+	}
+	
+	public Triangle[] ExtractTriangles()
+	{
+		Polygon Clone = Copy();
+		
+		ArrayList<Triangle> Triangles = new ArrayList<Triangle>(); 
+		
+		boolean Done = false;
+		
+		System.out.println("Extracting Triangles from Polygon");
+		
+		while (!Done) 
+		{
+			
+			ArrayList<Integer> ConcavePoints = new ArrayList<Integer>();
+			
+			Vector2f[] CornerList = Corners.ToArray();
+			
+			for (int i = 0; i < CornerList.length; i++)
+			{
+				if (PointConcave(i))
+				{
+					System.out.println("Found a concave point! It was at " + i);
+					ConcavePoints.add(i);
+				}
+			}
+
+			boolean[] UsedList = new boolean[CornerList.length];
+			
+			for (int i = 0; i < CornerList.length; i++) 
+			{
+				if (UsedList[i] == false && !ConcavePoints.contains(i))
+				{
+					int PreviousCorner;
+					int NextCorner;
+
+					if (i == 0) 
+					{
+						PreviousCorner = Corners.ToArray().length - 1;
+						NextCorner = 1;
+					}
+
+					else if (i == Corners.ToArray().length - 1) 
+					{
+						PreviousCorner = i - 1;
+						NextCorner = 0;
+					}
+
+					else 
+					{
+						PreviousCorner = i - 1;
+						NextCorner = i + 1;
+					}
+
+					Triangle Tri = new Triangle(CornerList[PreviousCorner], CornerList[i], CornerList[NextCorner]);
+
+					System.out.println("Triangle made!");
+					
+					Triangles.add(Tri);
+
+					if (Clone.GetCornerCount() == 3)
+					{
+						Done = true;
+
+						Triangle[] TriangleArray = new Triangle[Triangles.size()];
+						TriangleArray = Triangles.toArray(TriangleArray);
+						
+						System.out.println("Finished extracting triangles! We had " + TriangleArray.length + " triangles!");
+						
+						return TriangleArray;
+					}
+					
+					Clone.Cut(Tri);
+					
+					UsedList[PreviousCorner] = true;
+					UsedList[i] = true;
+					UsedList[NextCorner] = true;
+				}
+			} 
+		}
+		
+		return null;
+	}
+	
+	public int GetCornerCount()
+	{
+		return Corners.ToArray().length;
+	}
+	
+	private boolean PointConcave(int Corner)
+	{
+		
+		int PreviousCorner;
+		int NextCorner;
+		
+		if (Corner == 0)
+		{
+			PreviousCorner = Corners.ToArray().length - 1;
+			NextCorner = 1;
+		}
+		
+		else if (Corner == Corners.ToArray().length - 1)
+		{
+			PreviousCorner = Corner - 1;
+			NextCorner = 0;
+		}
+		
+		else
+		{
+			PreviousCorner = Corner - 1;
+			NextCorner = Corner + 1;
+		}
+		
+		Vector2f[] CornerArray = Corners.ToArray();
+		
+		Vector2f AB = CornerArray[Corner].Derive();
+		AB.Subtract(CornerArray[PreviousCorner]);
+		AB = new Vector2f(-AB.GetY(), AB.GetX());
+		Vector2f BC = CornerArray[NextCorner].Derive();
+		BC.Subtract(CornerArray[Corner]);
+		
+		float DotProduct = (AB.GetX() * BC.GetX()) + (AB.GetY() * BC.GetY());
+
+		System.out.println("AB = " + AB.ToString());
+		System.out.println("BC = " + BC.ToString());
+		
+		System.out.println("The dot product was " + DotProduct);
+		
+		return (DotProduct) < 0;
+	}
+	
 	public Vector2f GetScale()
 	{
-		Vector2f BottemLeft = new Vector2f(Vector2fUtils.GetFurthestWest(Corners.ToArray()).GetX(), Vector2fUtils.GetFurthestSouth(Corners.ToArray()).GetY());
-		Vector2f TopRight = new Vector2f(Vector2fUtils.GetFurthestEast(Corners.ToArray()).GetX(), Vector2fUtils.GetFurthestNorth(Corners.ToArray()).GetY());
+		Vector2f BottemLeft = new Vector2f(Vector2fUtils.GetMinX(Corners.ToArray()).GetX(), Vector2fUtils.GetMinY(Corners.ToArray()).GetY());
+		Vector2f TopRight = new Vector2f(Vector2fUtils.GetMaxY(Corners.ToArray()).GetX(), Vector2fUtils.GetMaxY(Corners.ToArray()).GetY());
 
 		float PolygonWidth = BottemLeft.GetXDistanceTo(TopRight);
 		float PolygonHeight = BottemLeft.GetYDistanceTo(TopRight);
@@ -277,10 +481,8 @@ public class Polygon extends Shape
 	@Override
 	public void SetScale(Vector2f Scale)
 	{
-		float CurrentXScale = Math.abs(Vector2fUtils.GetFurthestEast(Corners.ToArray()).GetXDistanceTo(Vector2fUtils.GetFurthestWest(Corners.ToArray())));
-		float CurrentYScale = Math.abs(Vector2fUtils.GetFurthestNorth(Corners.ToArray()).GetXDistanceTo(Vector2fUtils.GetFurthestSouth(Corners.ToArray())));
-
-		Vector2f MultiplyBy = new Vector2f(Scale.GetX() - CurrentXScale, Scale.GetY() - CurrentYScale);
+		Vector2f CurrentScale = GetScale();
+		Vector2f MultiplyBy = new Vector2f(Scale.GetX() - CurrentScale.GetX(), Scale.GetY() - CurrentScale.GetY());
 
 		Corners.Multiply(MultiplyBy);
 
@@ -303,5 +505,10 @@ public class Polygon extends Shape
 		Translation.Subtract(Center);
 		Position.Add(Translation);
 		// Corners.Add(Translation);
+	}
+
+	public Polygon Copy()
+	{
+		return new Polygon(Position, Corners.ToArray());
 	}
 }
