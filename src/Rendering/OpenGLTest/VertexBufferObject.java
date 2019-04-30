fe92e696754b064e2a60428e8b16fb9bfb1f985e
Michael Nix <47408522+NovaNix@users.noreply.github.com>
2019-04-30 15:27:36 -0500

    Removed unneeded methods

--- src/Rendering/OpenGLTest/VertexBufferObject.java
+++ src/Rendering/OpenGLTest/VertexBufferObject.java
@@ -20,40 +20,6 @@ public class VertexBufferObject
 		this.Indicies = I;
 	}
 
-	private void GenerateVBO(Polygon Poly)
-	{
-		this.Verticies = Vector2fUtils.GetCoordList(Poly.GetCompressed());
-		this.TextureVerticies = Vector2fUtils.GetCoordList(this.GenerateTexCoords(Poly));
-	}
-
-	private Vector2f[] GenerateTexCoords(Polygon Poly)
-	{
-
-		Vector2f[] PolygonCorners = Poly.GetCorners();
-
-		Vector2f PolygonScale = Poly.GetScale();
-
-		float PolygonWidth = PolygonScale.GetX();
-		float PolygonHeight = PolygonScale.GetY();
-
-		Vector2f Translation = PolygonScale.Derive().Divide(new Vector2f(2, 2));
-
-		Vector2f[] TexCoords = new Vector2f[PolygonCorners.length];
-
-		for (int i = 0; i < PolygonCorners.length; i++)
-		{
-			Vector2f Point = PolygonCorners[i].Derive();
-
-			Point.Subtract(Translation);
-
-			Point.Divide(PolygonScale);
-
-			TexCoords[i] = Point;
-		}
-
-		return TexCoords;
-	}
-
 	public float[] GetVerticies()
 	{
 		return this.Verticies;
