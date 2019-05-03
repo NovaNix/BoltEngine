3c0c3a886d5b153f0c36c83b8ba341430ae04ef9
michael <michael@Nova>
2019-05-02 22:45:20 -0500

    Fixed errors

--- src/Rendering/Masking/Mask.java
+++ src/Rendering/Masking/Mask.java
@@ -1,15 +1,15 @@
 /*
- * 
+ *
  */
 package Rendering.Masking;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
 
-import javax.swing.ImageIcon;
+import org.lwjgl.BufferUtils;
 
+import Rendering.OpenGLTest.Texture;
 import Vectors.Vector2f;
 
 public class Mask extends Texture
@@ -21,22 +21,22 @@ public class Mask extends Texture
 
 	public Mask(BufferedImage MaskingTexture)
 	{
-		super(GenerateMask(MaskingTexture, false), new Vector2f(MaskingTexture.getWidth(), MaskingTexture.getHeight());
-		
-		this.MaskingTexture = MaskingTexture;
+		super(GenerateMask(MaskingTexture, false), new Vector2f(MaskingTexture.getWidth(), MaskingTexture.getHeight()));
+
+		this.MaskTexture = MaskingTexture;
 	}
 
 	public Mask(BufferedImage MaskingTexture, boolean Inverted)
 	{
-		super(GenerateMask(MaskingTexture, Inverted), new Vector2f(MaskingTexture.getWidth(), MaskingTexture.getHeight());
-		
+		super(GenerateMask(MaskingTexture, Inverted), new Vector2f(MaskingTexture.getWidth(), MaskingTexture.getHeight()));
+
 		this.MaskTexture = MaskingTexture;
 		this.Inverted = Inverted;
 	}
 
-	public ByteBuffer GenerateMask(BufferedImage Mask, boolean Inverted)
+	public static ByteBuffer GenerateMask(BufferedImage Mask, boolean Inverted)
 	{
-		ByteBuffer Buffer = BufferUtils.createByteBuffer(Extract.getWidth() * Extract.getHeight() * 4);
+		ByteBuffer Buffer = BufferUtils.createByteBuffer(Mask.getWidth() * Mask.getHeight() * 4);
 
 		for (int y = 0; y < Mask.getHeight(); y++)
 		{
@@ -47,31 +47,30 @@ public class Mask extends Texture
 				if (Inverted)
 				{
 					// Put all red because R, G, and B should all be the same
-					
+
 					// The "255 - " is there to invert the colors
-					
-					Buffer.put((byte) 255 - PixelColor.getRed());
-					Buffer.put((byte) 255 - PixelColor.getRed());
-					Buffer.put((byte) 255 - PixelColor.getRed());
-					Buffer.put(255);
+
+					Buffer.put((byte) (255 - PixelColor.getRed()));
+					Buffer.put((byte) (255 - PixelColor.getRed()));
+					Buffer.put((byte) (255 - PixelColor.getRed()));
+					Buffer.put((byte) 255);
 				}
 
 				else
 				{
 					// Put all red because R, G, and B should all be the same
-	
+
 					Buffer.put((byte) PixelColor.getRed());
 					Buffer.put((byte) PixelColor.getRed());
 					Buffer.put((byte) PixelColor.getRed());
-					Buffer.put(255);
+					Buffer.put((byte) 255);
 				}
 			}
 		}
 
 		Buffer.flip();
-		
+
 		return Buffer;
 	}
-
 
 }
