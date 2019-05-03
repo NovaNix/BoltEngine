81c2a1d71bb301015aba03d7674d6282be12bb77
michael <michael@Nova>
2019-05-03 18:40:10 -0500

    Fixed Errors

--- src/Engine/BoltMath.java
+++ src/Engine/BoltMath.java
@@ -1,12 +1,13 @@
 package Engine;
 
+import Vectors.Vector2f;
+
 public class BoltMath
 {
 
 	public static boolean PointIsConcave(Vector2f Point, Vector2f PreviousPoint, Vector2f NextPoint)
 	{
-		
+		return false;
 	}
-
 
 }
--- src/Rendering/OpenGLTest/Window.java
+++ src/Rendering/OpenGLTest/Window.java
@@ -1,18 +1,40 @@
 package Rendering.OpenGLTest;
 
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWWindowSizeCallbackI;
-
-//import org.jogamp.glg2d.GLG2DCanvas;
-
-import Vectors.Vector2f;
-
+import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MAJOR;
+import static org.lwjgl.glfw.GLFW.GLFW_CONTEXT_VERSION_MINOR;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_CORE_PROFILE;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_FORWARD_COMPAT;
+import static org.lwjgl.glfw.GLFW.GLFW_OPENGL_PROFILE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_SAMPLES;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwHideWindow;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowIcon;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowSizeCallback;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_TRUE;
 import static org.lwjgl.opengl.GL11.glClear;
 import static org.lwjgl.system.MemoryUtil.NULL;
-import static org.lwjgl.glfw.GLFW.*;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWImage;
+import org.lwjgl.glfw.GLFWWindowSizeCallbackI;
+
+import IO.EasyLoader;
+
+//import org.jogamp.glg2d.GLG2DCanvas;
+
+import Vectors.Vector2f;
 
 public class Window implements GLFWWindowSizeCallbackI
 {
@@ -51,7 +73,7 @@ public class Window implements GLFWWindowSizeCallbackI
 		GLFWImage.Buffer GLFWBuffer = GLFWImage.create(1);
 		GLFWImage GLFWIcon = GLFWImage.create().set(Icon.GetWidth(), Icon.GetHeight(), Icon.GetData());
 		GLFWBuffer.put(0, GLFWIcon);
-		
+
 		glfwSetWindowIcon(WindowHandle, GLFWBuffer);
 
 	}
