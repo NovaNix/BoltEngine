71a585cbe101cba8757d8f79fab633c3cd4788f8
Michael Nix <47408522+NovaNix@users.noreply.github.com>
2019-05-03 14:46:28 -0500

    Added the ability to get corner angles

--- src/Geometry/Shapes/Triangle.java
+++ src/Geometry/Shapes/Triangle.java
@@ -10,6 +10,36 @@ public class Triangle extends Polygon
 		super(new Vector2f[] {Corner1, Corner2, Corner3});
 	}
 	
+	public float GetAngleOfCorner1()
+	{
+		return AngleBetween(GetCorner2(), GetCorner1(), GetCorner3());
+	}
+	
+	public float GetAngleOfCorner2()
+	{
+		return AngleBetween(GetCorner1(), GetCorner2(), GetCorner3());
+	}
+	
+	public float GetAngleOfCorner3()
+	{
+		return AngleBetween(GetCorner1(), GetCorner3(), GetCorner2());
+	}
+	
+	private float AngleBetween(Vector2f Point1, Vector2f Intersection, Vector2f Point2)
+	{
+		Vector2f AB = Intersection.Derive();
+
+		AB.Subtract(Point1);
+		
+		Vector2f BC = Point2;
+
+		BC.Subtract(Intersection);
+
+		float DotProduct = AB.DotProduct(BC);
+		
+		return Math.toDegrees(Math.acos(DotProduct));
+	}
+	
 	public Vector2f GetCorner1()
 	{
 		return Corners.ToArray()[0];
