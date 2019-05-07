3bbf9ab41b2adb484bad4b69af162e65ebfa2d60
Michael Nix <47408522+NovaNix@users.noreply.github.com>
2019-05-07 14:01:41 -0500

    Update SortingAlgorithm.java

--- src/Algorithms/Sorting/SortingAlgorithm.java
+++ src/Algorithms/Sorting/SortingAlgorithm.java
@@ -8,12 +8,39 @@ import java.util.Comparator;
 
 import Algorithms.Algorithm;
 
-public abstract class SortingAlgorithm implements Algorithm
+public abstract class SortingAlgorithm <H> extends Algorithm
 {
 
-	public Object[] Execute(Object[] Parameters)
+	boolean Done = false;
+
+	ArrayList<H> ToSort;
+	Comparator<H> Testing;
+
+	ArrayList<H> Sorted;
+
+	public SortingAlgorithm(ArrayList<H> ToSort, Comparator<H> Testing)
 	{
-		return new Object[] {};
+		this.ToSort = ToSort;
+		this.Testing = Testing;
+	}
+
+	@Override
+	public Object[] GetResult()
+	{
+		if (Done)
+		{
+			return new Object[] {Sorted};
+		}
+		
+		return null;
+	}
+
+	@Override
+	public void run()
+	{
+		Sorted = Sort(ToSort, Testing);
+		
+		Done = true;
 	}
 
 	public abstract <H> ArrayList<H> Sort(ArrayList<H> ToSort, Comparator<H> Testing);
