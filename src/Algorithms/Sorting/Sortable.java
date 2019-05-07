294252dd788b65fb4a0c8df5f8fda33bbfdc4434
Michael Nix <47408522+NovaNix@users.noreply.github.com>
2019-05-07 14:07:53 -0500

    Update Sortable.java

--- src/Algorithms/Sorting/Sortable.java
+++ src/Algorithms/Sorting/Sortable.java
@@ -8,6 +8,12 @@ import java.util.Comparator;
 public interface Sortable
 {
 
+	public <H> void Sort(Algorithm Alg)
+	{
+		
+	}
+		
+	
 	public <H> void Sort(SortingAlgorithm Algorithm, Comparator<H> Tester);
 
 }
