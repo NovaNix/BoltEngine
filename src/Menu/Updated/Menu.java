b74b43afed94214528e7e96cb613baaa4ac12499
Michael Nix <47408522+NovaNix@users.noreply.github.com>
2019-05-15 14:17:48 -0500

    Create Menu.java

---
+++ src/Menu/Updated/Menu.java
@@ -0,0 +1,37 @@
+package Menu.Updated;
+
+public class Menu
+{
+
+	String Name;
+	Vector2f Size;
+	
+	MenuScreen ActiveScreen;
+	
+	ArrayList<MenuScreen> Screens = new ArrayList<MenuScreen>();
+
+	public Menu(String Name)
+	{
+		this.Name = Name;
+	
+	}
+
+	public void Update()
+	{
+		ActiveScreen.Update();
+	}
+
+	public void SetSize(Vector2f Size)
+	{
+		this.Size = Size;
+		
+		for (int i = 0; i < Screens.size(); i++)
+		{
+			Screens.get(i).SetSize(Size);
+		}
+	}
+
+
+
+
+}
