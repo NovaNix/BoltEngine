e1cc4291b3eb47c0dee5cf3e72bb33549c7a885f
michael <michael@Nova>
2019-05-16 20:14:34 -0500

    Fixed Errors

--- src/Sound/LocalSound.java
+++ src/Sound/LocalSound.java
@@ -1,12 +1,14 @@
 package Sound;
 
-public class LocalSound
+import Vectors.Vector2f;
+
+public class LocalSound extends Sound
 {
 
 	Vector2f Location;
 	AudioObserver Observer;
 
-	public AmbientSound(String AudioPath, Vector2f Location, AudioObserver Observer)
+	public LocalSound(String AudioPath, Vector2f Location, AudioObserver Observer)
 	{
 		super(AudioPath);
 		this.Location = Location;
@@ -16,15 +18,15 @@ public class LocalSound
 	@Override
 	public void Update()
 	{
-		float Distance = Location.DistanceTo(Observer.GetObserverPosition();
-	
+		float Distance = Location.GetDistanceTo(Observer.GetObserverPosition());
+
 		float Volume = 500 - Distance;
-		
+
 		if (Volume < 0)
 		{
 			Volume = 0;
 		}
-	
+
 		SetVolume(Volume);
 
 	}
