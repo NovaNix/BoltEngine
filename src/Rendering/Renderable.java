09de8bb184b55c0bbd0076d77b73d543291d2078
michael <michael@Nova>
2019-05-28 00:51:58 -0500

    Documented

--- src/Rendering/Renderable.java
+++ src/Rendering/Renderable.java
@@ -1,21 +1,22 @@
-/*
- *
- */
 package Rendering;
 
 import java.awt.Color;
 
 import Rendering.Rendering.RenderingType;
 
+// Specified that something can be rendered
 public interface Renderable
 {
+	// Renders the object
 	void Render();
 
+	// Renders the object a specific color
 	default void Render(Color Hue)
 	{
 		Render();
 	}
 
+	// Renders the object a specific color and renderingtype
 	default void Render(Color Hue, RenderingType Type)
 	{
 		Render();
