ad1a327ff498e4c34fa0ea64fc938616131c87b8
michael <michael@Nova>
2019-06-06 03:54:08 -0500

    Added quality of life changes

--- src/Tile/ImageToTile.java
+++ src/Tile/ImageToTile.java
@@ -4,6 +4,11 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
+import IO.File.EasyLoader;
+import Tile.Storage.Tile;
+import Tile.Storage.TileMap;
+import Vectors.Vector2f;
+
 public class ImageToTile
 {
 
@@ -25,6 +30,42 @@ public class ImageToTile
 		}
 
 		return TileArray;
+	}
+
+	public static TileMap LoadTiles(HashMap<Color, Tile> Key, BufferedImage TileImage)
+	{
+		Color[][] Colors = GetColors(TileImage);
+
+		TileMap Tiles = new TileMap(TileImage.getWidth(), TileImage.getHeight(), TileMap.ORIGIN_TOP_LEFT);
+
+		for (int x = 0; x < TileImage.getWidth(); x++)
+		{
+			for (int y = 0; y < TileImage.getHeight(); y++)
+			{
+				Tiles.SetTile(x, y, new Tile(Key.get(Colors[x][y]), new Vector2f(x, y)));
+			}
+		}
+
+		return Tiles;
+	}
+
+	public static TileMap LoadTiles(HashMap<Color, Tile> Key, String TileImagePath)
+	{
+		BufferedImage TileImage = EasyLoader.LoadLocalImage(TileImagePath);
+
+		Color[][] Colors = GetColors(TileImage);
+
+		TileMap Tiles = new TileMap(TileImage.getWidth(), TileImage.getHeight(), TileMap.ORIGIN_TOP_LEFT);
+
+		for (int x = 0; x < TileImage.getWidth(); x++)
+		{
+			for (int y = 0; y < TileImage.getHeight(); y++)
+			{
+				Tiles.SetTile(x, y, new Tile(Key.get(Colors[x][y]), new Vector2f(x, y)));
+			}
+		}
+
+		return Tiles;
 	}
 
 	private static Color[][] GetColors(BufferedImage TileImage)
