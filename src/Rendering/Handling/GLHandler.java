90c87a27d701f3287363740d282c4f5406dd253e
michael <michael@Nova>
2019-06-11 00:58:19 -0500

    Added some comments

--- src/Rendering/Handling/GLHandler.java
+++ src/Rendering/Handling/GLHandler.java
@@ -1,6 +1,13 @@
 package Rendering.Handling;
 
+// This class makes it possible to make and destroy OpenGL things in any thread 
 public class GLHandler
 {
+
+	// Threading
+
+	// Creation
+
+	// Trash Handling
 
 }
